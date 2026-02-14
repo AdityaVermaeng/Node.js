004bf5908e2e4270a083a5d88a6df22d18aec14e
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-02-14 23:17:10 +0530

    feat:create , write, read and also reanme the file using fs

--- Day3/commandLine.js
+++ Day3/commandLine.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 // fs.mkdirSync("Demo");
 // fs.writeFileSync("");
 // fs.writeFileSync(Demo/${process.argv[2], process.argv[3]);
-// fs.renameSync(process.argv[2], process.argv[3]);
+fs.renameSync(process.argv[2], process.argv[3]);
 // let a = fs.readFileSync(`Demo/${process.argv[2]}`, "utf-8");
 // console.log(a);
 if (process.argv[2] % 2 === 0) console.log("Even")
