8fdacbf43f4df077b92bb0722eb7a1e19aac253b
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-02-14 23:14:06 +0530

    feat:send the input from command line and check the validation

---
+++ Day3/commandLine.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+// fs.mkdirSync("Demo");
+// fs.writeFileSync("");
+// fs.writeFileSync(Demo/${process.argv[2], process.argv[3]);
+// fs.renameSync(process.argv[2], process.argv[3]);
+// let a = fs.readFileSync(`Demo/${process.argv[2]}`, "utf-8");
+// console.log(a);
+if (process.argv[2] % 2 === 0) console.log("Even")
+else console.log("odd");
