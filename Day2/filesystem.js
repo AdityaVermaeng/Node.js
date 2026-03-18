8116acda69deb766f4442b1ca299e119023795b9
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-03-18 16:49:21 +0530

    feat: add file System concepts(update,rename and delete ) in node

--- Day2/filesystem.js
+++ Day2/filesystem.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const pathDir = path.join(__dirname, "crud");
-const filePath = `${pathDir}/demo.txt`; // create a file name demo.txt
+const filePath = `${pathDir}/demo2.txt`; // create a file name demo.txt
 console.log(filePath);
 
 //-----------------Create---------------------//
@@ -11,6 +11,28 @@ fs.writeFileSync(filePath, "this is crud") //this way and above wala same hain .
 
 
 //---------------------read-------------------//
+// fs.readFile(filePath, (err, item) => {
+//         if (!err) console.log(item);
+// }) // node contains his own buffer(Datatype) which user canot read the existing info so we use utf8 that user can easily read nad understand
 fs.readFile(filePath, 'utf8', (err, item) => {
     console.log(item);
 })
+
+
+//---------------Update file data -----------------------//
+
+fs.appendFile(filePath, 'file name is crud', (err) => {
+    if (!err) console.log("file updated")
+})
+
+
+
+//-----------------file rename-----------//
+fs.rename(filePath, `${pathDir}/demo3.txt`, (err) => {
+    if (!err) console.log("File is renamed")
+});
+
+
+
+//---------------------delete file-------------------//
+fs.unlinkSync(`${pathDir}/demo3.txt`);
