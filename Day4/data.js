06d6eda0065368cbf3e9e04148c5633d312dac0c
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-04-04 14:26:41 +0530

    feat:create a data file and add data for used in simpleApi.js

---
+++ Day4/data.js
@@ -0,0 +1,8 @@
+// const data={"name":"Aditya","age":"22","city":"Delhi","country":"India"
+// };
+// module.exports=data;
+
+//we can use data in array format as well
+const data=[
+	{"name":"Aditya","age":"22","city":"Delhi","country":"India"}];
+module.exports=data;
