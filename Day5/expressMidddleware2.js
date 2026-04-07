170f103c308987958b40f9d625590bec8b0893b1
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-04-07 23:24:21 +0530

    feat: add route  level middleware through express routes

--- Day5/expressMidddleware2.js
+++ Day5/expressMidddleware2.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const app = express();
+
+
+const reqFilter = ((req, res, next) => {
+	if(!req.query.age) {
+		res.send("Please provide age");
+	}
+	else if(req.query.age < 18) {
+		res.send("You cannot access this page");
+	}
+	else{
+		next();
+	}
+})
+app.get('/',(req,res)=>{
+	res.send("Welcome to Home Page");
+});
+
+app.get('/users',reqFilter,(req,res)=>{
+	res.send("Welcome to Users Page");
+});
+app.get('/about',(req,res)=>{
+	res.send("Welcome to About Page");
+})
+
+.listen(5000,()=>{
+	console.log("Server is running at port 5000");
+});
