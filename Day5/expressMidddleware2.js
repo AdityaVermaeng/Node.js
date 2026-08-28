31d2eb8ede55873a9afc2fac19bd6243f3411892
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-08-28 14:33:44 +0530

    feat:add a route

--- Day5/expressMidddleware2.js
+++ Day5/expressMidddleware2.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+const reqFilter = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please provide age");
+  } else if (req.query.age < 18) {
+    res.send("You cannot access this page");
+  } else {
+    next();
+  }
+};
 
-const reqFilter = ((req, res, next) => {
-	if(!req.query.age) {
-		res.send("Please provide age");
-	}
-	else if(req.query.age < 18) {
-		res.send("You cannot access this page");
-	}
-	else{
-		next();
-	}
-})
-app.get('/',(req,res)=>{
-	res.send("Welcome to Home Page");
+reqFilter();
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page");
 });
 
-app.get('/users',reqFilter,(req,res)=>{
-	res.send("Welcome to Users Page");
+app.get("/users", reqFilter, (req, res) => {
+  res.send("Welcome to Users Page");
 });
-app.get('/about',(req,res)=>{
-	res.send("Welcome to About Page");
-})
+app
+  .get("/about", (req, res) => {
+    res.send("Welcome to About Page");
+  })
 
-.listen(5000,()=>{
-	console.log("Server is running at port 5000");
-});
+  .listen(4000, () => {
+    console.log("Server is running at port 4000");
+  });
